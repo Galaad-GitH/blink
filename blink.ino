9c5464b725099bacb388a9254dfb85aecdb73afc
Guillaume Caron <Guillaume.Caron@OutLook.Com>
2026-02-14 22:50:39 +0100

    2.5 seconds is better

--- blink.ino
+++ blink.ino
@@ -1,6 +1,6 @@
 /*
   Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+  Turns on an LED on for one second, then off for 2.5 second, repeatedly.
  
   This example code is in the public domain.
  */
@@ -18,7 +18,7 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
+  delay(2500);               // Wait for a 2.5 second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);               // wait for a second
+  delay(2500);               // wait for a 2.5 second
 }
